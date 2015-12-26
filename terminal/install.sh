e80bcf4cad9397941919c11e2b7215b6752f3880
Hugo Martins <caramelo.martins@gmail.com>
2015-12-26 23:38:44 +0000

    changed terminal scripts and filenames

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,14 +1,23 @@
-# !/bin/bash
-# Install script for Terminal settings.
+#!/usr/bin/env bash
 
-# Source: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+#-------------------------------------------------------------------------------
+# Install script for Terminal settings.
+#-------------------------------------------------------------------------------
+
+##
+# Source:
+#   http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+##
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-for FILE in $(find -type f -name "bash*"); do
+find -type f -name "*.lnk" |
+while read FILE
+do
+  FILE=${FILE%.*} # Set correct file name!
   if [ ! -h "$HOME/.${FILE##*/}" ]; then
-    echo "- Installing $HOME/.${FILE##*/}..."
-    ln -s "$DIR/${FILE##*/}" "$HOME/.${FILE##*/}"
+    echo "- Installing $HOME/.${FILE##*/}!"
+    ln -s "$DIR/${FILE##*/}.lnk" "$HOME/.${FILE##*/}"
   else
     echo "- $HOME/.${FILE##*/} already exists!"
   fi
--- terminal/remove.sh
+++ terminal/remove.sh
@@ -1,11 +1,20 @@
-# !/bin/bash
-# Remove all dotfiles with Terminal settings.
+#!/usr/bin/env bash
 
-# Source: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+#-------------------------------------------------------------------------------
+# Remove all dotfiles with Terminal settings.
+#-------------------------------------------------------------------------------
+
+##
+# Source:
+#   http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+##
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-for FILE in $(find -type f -name "bash*"); do
+find -type f -name "*.lnk" |
+while read FILE
+do
+  FILE=${FILE%.*} # Set correct file name!
   if [ -h "$HOME/.${FILE##*/}" ]; then
     echo "- Removing $HOME/.${FILE##*/}!"
     rm "$HOME/.${FILE##*/}"
