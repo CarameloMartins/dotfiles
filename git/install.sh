efa7d3a36123eae03097f9efa067e4708c017871
Hugo Martins <caramelo.martins@gmail.com>
2015-12-31 02:04:05 +0000

    removed git config from auto isntallation due to bug

--- git/install.sh
+++ git/install.sh
@@ -22,17 +22,17 @@ fi
 #-------------------------------------------------------------------------------
 # Install gitconfig.
 #-------------------------------------------------------------------------------
-if [ ! -f "$HOME/.gitconfig" ]; then
-  echo "Installing .gitconfig."
-  cp "$DIR/gitconfig.cp" "$HOME/.gitconfig"
-
-  # Set Git name.
-  read -p "What's your Git name? " NAME
-  sed -i "s/\[GIT-NAME]/\$NAME/g" "$HOME/.gitconfig"
-
-  # Set Git email.
-  read -p "What's your Git email? " EMAIL
-  sed -i "s/\[GIT-EMAIL]/\$EMAIL/g" "$HOME/.gitconfig"
-else
-  echo "A gitconfig file was already found."
-fi
+#if [ ! -f "$HOME/.gitconfig" ]; then
+#  echo "Installing .gitconfig."
+#  cp "$DIR/gitconfig.cp" "$HOME/.gitconfig"
+#
+#  # Set Git name.
+#  read -p "What's your Git name? " NAME
+#  sed -i "s/\[GIT-NAME]/\$NAME/g" "$HOME/.gitconfig"
+#
+#  # Set Git email.
+#  read -p "What's your Git email? " EMAIL
+#  sed -i "s/\[GIT-EMAIL]/\$EMAIL/g" "$HOME/.gitconfig"
+#else
+#  echo "A gitconfig file was already found."
+#fi
