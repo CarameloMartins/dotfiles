33d310945f31678cdfd8dbf23c01ebdcffa36ffb
Hugo Martins <caramelo.martins@gmail.com>
2016-02-13 23:25:34 +0000

    added capabilities to install .gitconfig but it is still unstable

--- git/install.sh
+++ git/install.sh
@@ -9,30 +9,31 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-#-------------------------------------------------------------------------------
+##
 # Install global .gitignore.
-#-------------------------------------------------------------------------------
+##
 if [ ! -h "$HOME/.gitignore" ]; then
   echo "- Installing global .gitignore."
-  ln -s "$DIR/gitignore.lnk" "$HOME/.gitignore"
+  ln -s "$DIR/gitignore.symlink" "$HOME/.gitignore"
 else
   echo "- A global .gitignore file already exists."
 fi
 
-#-------------------------------------------------------------------------------
+##
 # Install gitconfig.
-#-------------------------------------------------------------------------------
-#if [ ! -f "$HOME/.gitconfig" ]; then
-#  echo "Installing .gitconfig."
-#  cp "$DIR/gitconfig.cp" "$HOME/.gitconfig"
-#
-#  # Set Git name.
-#  read -p "What's your Git name? " NAME
-#  sed -i "s/\[GIT-NAME]/\$NAME/g" "$HOME/.gitconfig"
-#
-#  # Set Git email.
-#  read -p "What's your Git email? " EMAIL
-#  sed -i "s/\[GIT-EMAIL]/\$EMAIL/g" "$HOME/.gitconfig"
-#else
-#  echo "A gitconfig file was already found."
-# fi
+##
+
+if [ ! -f "$HOME/.gitconfig" ]; then
+  echo "- Installing .gitconfig."
+  cp "$DIR/gitconfig.copy" "$HOME/.gitconfig"
+
+  # Set Git name.
+  read -p "  - What's your Git name? " NAME
+  sed -i "s/\[GIT-NAME]/'$NAME'/g" "$HOME/.gitconfig"
+
+  # Set Git email.
+  read -p "  - What's your Git email? " EMAIL
+  sed -i "s/\[GIT-EMAIL]/'$EMAIL'/g" "$HOME/.gitconfig"
+else
+  echo "- A gitconfig file already exists."
+ fi
--- git/remove.sh
+++ git/remove.sh
@@ -9,9 +9,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-#-------------------------------------------------------------------------------
+##
 # Remove global .gitignore.
-#-------------------------------------------------------------------------------
+##
 if [ -h "$HOME/.gitignore" ]; then
   echo "- Removing global .gitignore."
   rm "$HOME/.gitignore"
@@ -20,6 +20,9 @@ else
   echo "- Global .gitignore is not currently installed."
 fi
 
+##
+# Remove -gitconfig file.
+##
 if [ -f "$HOME/.gitconfig" ]; then
   echo "- Removing gitconfig."
   rm "$HOME/.gitconfig"
