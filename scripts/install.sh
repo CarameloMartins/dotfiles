25211c3c69017955fe974ead31cb9b2414e57459
Hugo Martins <caramelomartins@master01>
2016-03-03 01:29:36 +0000

    removed all install.sh except generic install which now holds all installation instructions

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,71 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-for FILE in $(find .. -type f -name "*install.sh" | grep -v "scripts"); do
-  echo "Running '${FILE%.*}'."
-  ${FILE}
-done
+DOTFILES=${DIR%/*}
+
+##
+# Install gitconfig with correct configurations.
+##
+if [ ! -f "$HOME/.gitconfig" ]; then
+  echo "- Installing .gitconfig."
+  cp "$DOTFILES/git/gitconfig.copy" "$HOME/.gitconfig"
+
+  # Set Git name.
+  read -p "  - What's your Git name? " NAME
+  sed -i "s/\[GIT-NAME]/'$NAME'/g" "$HOME/.gitconfig"
+
+  # Set Git email.
+  read -p "  - What's your Git email? " EMAIL
+  sed -i "s/\[GIT-EMAIL]/'$EMAIL'/g" "$HOME/.gitconfig"
+else
+  echo "- A gitconfig file already exists."
+fi
+
+##
+# Symlink files with `symlink` extension to $HOME folder.
+##
+echo "- Starting to link files to \"$HOME\":"
+
+while IFS= read -r FILE
+do
+  FILE=${FILE%.*} # Set correct file name!
+  FILEPATH=$(readlink -f "$FILE.symlink")
+
+  if [ ! -h "$HOME/.${FILE##*/}" ]; then
+    echo "  - Installing $HOME/.${FILE##*/}."
+    ln -s "$FILEPATH" "$HOME/.${FILE##*/}"
+  else
+    echo "  - $HOME/.${FILE##*/} already exists."
+  fi
+done < <(find .. -type f -name "*.symlink")
+
+##
+# Install ~/.bin folder and copy needed files.
+##
+# Install ~/.bin if none exists.
+echo "- Installing ~./bin folder."
+
+if [ ! -d "$HOME/.bin" ]; then
+  echo "  - Folder \$HOME/.bin doesn't exist. Creating it."
+  mkdir "$HOME/.bin"
+else
+  echo "  - Folder \$HOME/.bin already exists."
+fi
+
+BIN="$HOME/.bin"
+
+# Copy custom scripts to ~/.bin.
+while IFS= read -r FILE
+do
+  FILE=${FILE%.*}
+  FILEPATH=$(readlink -f "$FILE.copy")
+
+  echo "  - Checking $BIN/${FILE##*/}."
+  if [ ! -f "$BIN/${FILE##*/}" ]; then
+    echo "   - Installing $BIN/${FILE##*/}."
+    cp "$FILEPATH" "$BIN/${FILE##*/}"
+    chmod +x "$BIN/${FILE##*/}"
+  else
+    echo "   - ${FILE##*/} already exists."
+  fi
+done <  <(find ../bin -type f -name "*.copy")
