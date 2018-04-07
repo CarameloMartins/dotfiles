965666ba4ac2a32c8c3202a598ab9a3c2287d467
Hugo Martins <caramelo.martins@gmail.com>
2018-04-08 00:33:41 +0100

    Add a small setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Bash
+ln -s $PWD/bash/bash_logout $HOME/.bash_logout
+ln -s $PWD/bash/bashrc $HOME/.bashrc
+
+# SSH
+ln -s $PWD/ssh/config $HOME/.ssh/config
+
+# tmux
+ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
+
+# VS Code
+ln -s $PWD/vscode/settings.json $HOME/.config/Code/User/settings.json
+
