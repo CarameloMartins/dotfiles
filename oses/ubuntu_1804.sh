ef34190cf7cfb878d3e1472a2f75ea4bc7c3001e
Hugo Martins <caramelo.martins@gmail.com>
2018-11-06 09:34:54 +0000

    Update ubuntu_1804.sh

--- oses/ubuntu_1804.sh
+++ oses/ubuntu_1804.sh
@@ -67,6 +67,9 @@ apt install postgresql-client
 # Development
 apt install docker-ce
 
+# Appearance
+apt install adwaita-icon-theme-full
+
 # Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
