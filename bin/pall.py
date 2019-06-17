9f8e9ac256dee6e2f8027dc9948bb333e124c8fd
Hugo Martins <caramelo.martins@gmail.com>
2019-06-17 16:43:03 +0100

    Modify pall.py and VSCode settings

--- bin/pall.py
+++ bin/pall.py
@@ -10,6 +10,43 @@ import os
 import sys
 import subprocess
 
+from git import exc, Repo
+
+def iterate_folders(path):
+    if os.path.isdir(path):
+        dirs = os.listdir(path)
+
+
+        try:
+            repo = Repo(path)
+            branches = repo.branches
+
+            print("\033[36m" + path + ":\033[0m")
+            print("- {} branches.".format(len(branches)))
+
+            if repo.is_dirty():
+                print("\033[91m[WARN] Repo is dirty, not risking a pull!!\033[0m")
+            else:
+                for branch in branches:
+                    print("- Pulling \033[33m'{}'\033[0m branch.".format(branch.name))
+                    try:
+                        repo.git.checkout(branch.name)
+                        repo.git.pull()
+                    except Exception as ex:
+                        print("[Error] ", ex)
+
+        except exc.InvalidGitRepositoryError:
+            if os.path.isdir(path):
+                try:
+                    dirs = os.listdir(path)
+
+                    for dir in dirs:
+                        realpath = os.path.realpath(path + "/" + dir)
+                        iterate_folders(realpath)
+                except exc.InvalidGitRepositoryError:
+                    print("\033[91m[WARN] This doesn't seem to be a valid repository.\033[0m")
+        
+
 ##
 # Parse arguments.
 ##
@@ -17,22 +54,14 @@ if len(sys.argv) < 2:
     raise Exception('Please provide a path to act on.')
 
 path = os.path.realpath(sys.argv[1])
-dirs = os.listdir(path)
 
 print("Searching " + path + ".")
 
 ##
 # Iterate each folder.
 ##
-for dir in dirs:
-    realpath = os.path.realpath(path + "/" + dir)
-    print(dir + ":")
-    os.chdir(realpath)
 
-    if os.path.exists(".git/"):
-        output = subprocess.check_output(["git", "pull"])
-        print(output)
-    else:
-        print("NO git repo.")
+iterate_folders(path)
+
 
 print("Finished searching.")
